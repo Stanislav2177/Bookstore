738f57e8e30bb057c1e618d56dc05e5267d4e376
Stanislav <stanislav2177@gmail.com>
2023-09-25 19:31:38 +0300

    Generate book schema with hibernate and accessing queries with JPA

---
+++ src/main/java/com/project/bookstore/entity/Book.java
@@ -0,0 +1,34 @@
+package com.project.bookstore.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "description")
+    private String description;
+
+}
---
+++ src/main/java/com/project/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.project.bookstore.repository;
+
+import com.project.bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
