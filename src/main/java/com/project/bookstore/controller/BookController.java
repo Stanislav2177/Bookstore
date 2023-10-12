fce609b8554ce2510865e646c637088fc77b7500
Stanislav <stanislav2177@gmail.com>
2023-10-12 21:45:26 +0300

    Added slf4j logger

--- src/main/java/com/project/bookstore/controller/BookController.java
+++ src/main/java/com/project/bookstore/controller/BookController.java
@@ -4,6 +4,8 @@ import com.project.bookstore.entity.Book;
 import com.project.bookstore.exception.BookAlreadyExistException;
 import com.project.bookstore.exception.BookNotFoundException;
 import com.project.bookstore.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +20,7 @@ import java.util.List;
 public class BookController {
 
     private final BookService bookService;
+    private final Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     public BookController(BookService bookService) {
@@ -26,54 +29,68 @@ public class BookController {
 
     @PostMapping("/add")
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        logger.info("Received a request to add a new book: {}", book.getTitle());
         Book addedBook = bookService.addBook(book);
         if (addedBook != null) {
+            logger.info("Book added successfully: {}", addedBook.getId());
             return new ResponseEntity<>(addedBook, HttpStatus.CREATED);
         } else {
+            logger.error("Failed to add the book: {}", book.getTitle());
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @GetMapping("/pageable/list")
     public ResponseEntity<Page<Book>> listBooksPage(Pageable pageable) {
+        logger.info("Received a request to list books with pageable.");
         Page<Book> books = bookService.getBooks(pageable);
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Book>> listBooks(){
+    public ResponseEntity<List<Book>> listBooks() {
+        logger.info("Received a request to list all books.");
         return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        logger.info("Received a request to retrieve a book by ID: {}", id);
         Book book = bookService.searchBookById(id);
         if (book != null) {
+            logger.info("Book found: {}", book.getId());
             return new ResponseEntity<>(book, HttpStatus.OK);
         } else {
+            logger.warn("Book not found with ID: {}", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        logger.info("Received a request to delete a book by ID: {}", id);
         Book book = bookService.searchBookById(id);
 
         if (book != null) {
             bookService.deleteBook(id);
+            logger.info("Book deleted successfully: {}", id);
             return ResponseEntity.noContent().build();
         } else {
+            logger.warn("Book not found for deletion with ID: {}", id);
             return ResponseEntity.notFound().build();
         }
     }
 
     @GetMapping("/title")
-    public ResponseEntity<Book> getBookByTitle(@PathVariable String title){
+    public ResponseEntity<Book> getBookByTitle(@PathVariable String title) {
+        logger.info("Received a request to retrieve a book by title: {}", title);
         Book book = bookService.searchBookByTitle(title);
 
-        if(book != null){
+        if (book != null) {
+            logger.info("Book found by title: {}", title);
             return new ResponseEntity<>(book, HttpStatus.OK);
-        }else{
+        } else {
+            logger.warn("Book not found with title: {}", title);
             return ResponseEntity.notFound().build();
         }
     }
@@ -81,13 +98,16 @@ public class BookController {
     @GetMapping("/sorted")
     public ResponseEntity<String> getAllBooksSortedByPrice(
             @RequestParam(name = "parameter") String parameter) {
+        logger.info("Received a request to retrieve and sort books by price.");
         try {
             List<Book> sortedBooks = bookService.getAllBooksSortedByPrice(parameter);
             return ResponseEntity.ok(sortedBooks.toString());
         } catch (IllegalArgumentException e) {
+            logger.error("Invalid parameter for sorting: {}", parameter);
             return ResponseEntity.badRequest()
                     .body("Invalid parameter. Use 'min' or 'max'.");
         } catch (Exception e) {
+            logger.error("Failed to retrieve and sort books: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Failed to retrieve and sort books: " + e.getMessage());
         }
@@ -95,39 +115,46 @@ public class BookController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBook(@RequestBody Book newBook, @PathVariable Long id) {
+        logger.info("Received a request to update a book with ID: {}", id);
         Book updatedBook;
         try {
             updatedBook = bookService.updateBook(newBook, id);
+            logger.info("Book updated successfully: {}", id);
             return ResponseEntity.ok(updatedBook);
         } catch (BookAlreadyExistException e) {
+            logger.error("Book update failed due to a conflict: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
         } catch (BookNotFoundException e) {
+            logger.warn("Book not found for update with ID: {}", id);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 
     @GetMapping("/random")
     public ResponseEntity<Book> getARandomBook() {
+        logger.info("Received a request to retrieve a random book.");
         Book randomBook;
         try {
             randomBook = bookService.getARandomBook();
+            logger.info("Random book retrieved: {}", randomBook.getId());
             return ResponseEntity.ok(randomBook);
         } catch (BookNotFoundException e) {
+            logger.warn("No random book found.");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 
     @GetMapping("/genre/{genre}")
     public ResponseEntity<List<Book>> getAllBooksSortedByGenre(@PathVariable String genre) {
+        logger.info("Received a request to retrieve books by genre: {}", genre);
         List<Book> booksByGenre;
         try {
             booksByGenre = bookService.getAllBooksSortedByGenre(genre);
+            logger.info("Books retrieved by genre: {}", genre);
             return ResponseEntity.ok(booksByGenre);
         } catch (Exception e) {
+            logger.error("Failed to retrieve and sort books by genre: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
-
-
 }
-
--- src/main/java/com/project/bookstore/controller/ShoppingCartController.java
+++ src/main/java/com/project/bookstore/controller/ShoppingCartController.java
@@ -1,7 +1,10 @@
 package com.project.bookstore.controller;
+
 import com.project.bookstore.dto.OrderDto;
 import com.project.bookstore.dto.OrderItemProjectionDto;
 import com.project.bookstore.service.ShoppingCartService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.List;
 public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
+    private final Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
 
     @Autowired
     public ShoppingCartController(ShoppingCartService shoppingCartService) {
@@ -21,43 +25,61 @@ public class ShoppingCartController {
 
     @GetMapping("/view")
     public ResponseEntity<?> viewCart() {
+        logger.info("Received a request to view the shopping cart.");
         return ResponseEntity.ok(shoppingCartService.getCartContents());
     }
 
     @PostMapping("/add/{bookId}")
-    public ResponseEntity<?> addToCart(@PathVariable Long bookId,
-                                       @RequestParam int quantity) {
+    public ResponseEntity<?> addToCart(@PathVariable Long bookId, @RequestParam int quantity) {
+        logger.info("Received a request to add book with ID {} " +
+                "to the cart with quantity {}.", bookId, quantity);
+
         shoppingCartService.addToCart(bookId, quantity);
+
+        logger.info("Book with ID {} added to cart successfully.", bookId);
         return ResponseEntity.ok("Item added to cart successfully");
     }
 
-
-
     @GetMapping("/order-details")
-    public ResponseEntity<List<OrderItemProjectionDto>> getOrderDetails(){
-        List<OrderItemProjectionDto> orderItemProjectionDtos = shoppingCartService.customSelect();
+    public ResponseEntity<List<OrderItemProjectionDto> > getOrderDetails() {
 
-        System.out.println(orderItemProjectionDtos.toString());
+        logger.info("Received a request to get order details.");
 
-        return ResponseEntity.ok(shoppingCartService.customSelect());
+        List<OrderItemProjectionDto> orderItemProjectionDtos
+                = shoppingCartService.customSelect();
+
+        logger.info("Order details retrieved: {}", orderItemProjectionDtos.toString());
+        return ResponseEntity.ok(orderItemProjectionDtos);
     }
 
     @PostMapping("/remove/{bookId}")
     public ResponseEntity<?> removeFromCart(@PathVariable Long bookId, @RequestParam int quantity) {
+        logger.info("Received a request to remove book with ID {} " +
+                "from the cart with quantity {}.", bookId, quantity);
+
         shoppingCartService.removeFromCart(bookId, quantity);
+        logger.info("Book with ID {} removed from cart successfully.", bookId);
+
         return ResponseEntity.ok("Item removed from cart successfully");
     }
 
     @GetMapping("/totalPrice")
     public ResponseEntity<?> calculateTotalPrice() {
+        logger.info("Received a request to calculate the total price.");
+
         double totalPrice = shoppingCartService.calculateTotalPrice();
+
+        logger.info("Total Price calculated: {}", totalPrice);
         return ResponseEntity.ok("Total Price: " + totalPrice);
     }
 
     @PostMapping("/finishOrder")
     public ResponseEntity<OrderDto> finishOrder() {
+        logger.info("Received a request to finish the order.");
+
         OrderDto orderDto = shoppingCartService.finishOrder();
+
+        logger.info("Order finished successfully.");
         return ResponseEntity.ok(orderDto);
     }
 }
-
